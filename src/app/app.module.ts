97d072e81e91f9016578dcd542864ce2369616dc
dhinakar <dhinakartrivedula@gmail.com>
2024-08-15 12:41:52 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    SearchPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    HttpClientModule,
+    FormsModule,
+    MatPaginatorModule,
+    MatTableModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/search-page/search-page.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { UniversityService } from '../university.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html',
+  styleUrls: ['./search-page.component.css']
+})
+export class SearchPageComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  username: string = '';
+  countries: string[] = ['United States', 'United Kingdom', 'Canada', 'Australia', 'Germany', 'France', 'Japan', 'India'];
+  selectedCountry: string = '';
+  otherCountry: string = '';
+  searchCount: number = 0;
+  universities: any[] = [];
+  totalItems: number = 0;
+  pageSize: number = 10;
+  pageSizeOptions: number[] = [5, 10, 20, 50, 100];
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['name', 'stateProvince', 'webpages']
+  currentPage: number =0;
+  isLoading: boolean = false;
+
+  constructor(private router: Router, private universityService: UniversityService) { }
+
+  ngOnInit(): void {
+    if (localStorage) {
+      this.username = localStorage.getItem('username') || '';
+      const searchCount = localStorage.getItem('searchCount');
+    if (searchCount) {
+      this.searchCount = parseInt(searchCount);
+    }
+    } else {
+      this.username = '';
+      this.searchCount = 0;
+    }
+    this.dataSource = new MatTableDataSource<any>();
+  }
+
+  logout(): void {
+    localStorage.removeItem('username');
+    localStorage.removeItem('searchCount');
+    this.router.navigate(['/login']);
+  }
+
+  search(): void {
+    this.isLoading =true
+    let country = this.otherCountry ? this.otherCountry : this.selectedCountry;
+    this.universityService.getUniversities(country)
+      .subscribe((data: any) => {
+        this.universities = data.map((university: { [x: string]: any; name: any; web_pages: any; }) => ({
+          name: university.name,
+          stateProvince: university['state-province'],
+          webpages: university.web_pages
+        }));
+        this.dataSource.data = this.universities;
+        this.totalItems = this.universities.length;
+        console.log("items",this.totalItems, this.universities)
+        this.dataSource.paginator = this.paginator;
+        this.searchCount++;
+        localStorage.setItem('searchCount', this.searchCount.toString());
+        this.paginator.firstPage();
+        this.isLoading = false;
+      });
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.currentPage = event.pageIndex;
+    this.pageSize = event.pageSize;
+  }
+}
